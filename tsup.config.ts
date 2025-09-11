1159f64cad83dc3af11d6b777089b1b12965143d
Gilad Mucharer <giladm11@gmail.com>
2025-09-11 19:27:30 +0300

    adding bundled version

---
+++ tsup.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig([
+      {
+        entry: ["src/index.ts"],
+        format: ["cjs", "esm"],
+        dts: true,
+        sourcemap: true,
+      },
+    {
+        entry: ["src/index.ts"],
+        format: ["iife"],
+        globalName: "KaspaComSwapSdk",
+        dts: false,
+        sourcemap: true,
+        target: "es2020",
+        platform: "browser",
+        outDir: "dist", 
+    },
+]);
