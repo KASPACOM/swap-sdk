2c7eb3996cf2f367d3305b6f9427944a70f0c356
Gilad Mucharer <giladm11@gmail.com>
2025-09-29 00:02:32 +0300

    change kasplex testnet url

--- src/config/networks.ts
+++ src/config/networks.ts
@@ -5,7 +5,7 @@ export const NETWORKS: Record<string, SwapSdkNetworkConfig> = {
   'kasplex-testnet': {
     name: 'Kasplex Test',
     chainId: 167012,
-    rpcUrl: 'https://rpc.kasplextest.xyz',
+    rpcUrl: 'https://rpc.kspr.bot/kasplex/testnet',
     routerAddress: '0x81Cc4e7DbC652ec9168Bc2F4435C02d7F315148e',
     factoryAddress: '0x89d5842017ceA7dd18D10EE6c679cE199d2aD99E',
     proxyAddress: '0x5B7e7830851816f8ad968B0e0c336bd50b4860Ad',
