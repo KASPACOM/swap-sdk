ec588241752d19c91a6ee12530b5190da3c12c6e
Gilad Mucharer <giladm11@gmail.com>
2025-11-04 21:07:40 +0200

    update graphs

--- src/config/networks.ts
+++ src/config/networks.ts
@@ -9,7 +9,7 @@ export const NETWORKS: Record<string, SwapSdkNetworkConfig> = {
     routerAddress: '0x81Cc4e7DbC652ec9168Bc2F4435C02d7F315148e',
     factoryAddress: '0x89d5842017ceA7dd18D10EE6c679cE199d2aD99E',
     proxyAddress: '0x5B7e7830851816f8ad968B0e0c336bd50b4860Ad',
-    graphEndpoint: 'https://dev-graph-kasplex.kaspa.com/subgraphs/name/kasplex-testnet-new-v2-core',
+    graphEndpoint: 'https://dev-graph-kasplex.kaspa.com/subgraphs/name/kasplex-testnet-kas-new-v2-core',
     blockExplorerUrl: 'https://explorer.testnet.kasplextest.xyz',
     additionalJsonRpcApiProviderOptionsOptions: {
       batchMaxCount: 1,
@@ -36,7 +36,7 @@ export const NETWORKS: Record<string, SwapSdkNetworkConfig> = {
     routerAddress: '0x3a1f0bD164fe9D8fa18Da5abAB352dC634CA5F10',
     factoryAddress: '0xa9CBa43A407c9Eb30933EA21f7b9D74A128D613c',
     proxyAddress: '0x4c5BEaAE83577E3a117ce2F477fC42a1EA39A8a3',
-    graphEndpoint: 'https://graph-kasplex.kaspa.com/subgraphs/name/kasplex-v2-core',
+    graphEndpoint: 'https://graph-kasplex.kaspa.com/subgraphs/name/kasplex-kas-v2-core',
     blockExplorerUrl: 'https://explorer.kasplex.org',
     additionalJsonRpcApiProviderOptionsOptions: {
       batchMaxCount: 1,
